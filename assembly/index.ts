2fe6e8400ae388f4beb8915dfc71b5962531c09f
Buddhi Ranasinghe <Buddhi.Ranasinghe@cloudsolution-sa.com>
2024-12-31 15:46:15 +0530

    Test performance

--- assembly/index.ts
+++ assembly/index.ts
@@ -3,3 +3,12 @@
 export function add(a: i32, b: i32): i32 {
   return a + b;
 }
+
+export function testArray(data: i32[]): i32[] {
+  let a: i32[] = [];
+  const dataLength = data.length;
+  for (let i = 0; i < dataLength; i++) {
+    a.push(data[i] + 1);
+  }
+  return a;
+}
--- tests/index.js
+++ tests/index.js
@@ -1,4 +1,16 @@
 import assert from "assert";
-import { add } from "../build/debug.js";
+import { add, testArray } from "../build/debug.js";
 assert.strictEqual(add(1, 2), 3);
 console.log("ok");
+
+const data = Array.from({ length: 1000000 }, (_, i) => i);
+
+console.time("testArray");
+const a = testArray(data);
+console.log(a);
+console.timeEnd("testArray");
+
+console.time("default");
+const b = data.map((x) => x + 1);
+console.log(b);
+console.timeEnd("default");
